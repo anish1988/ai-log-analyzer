4e72a87f5eb2b905b3e9671c9099e5a5528c67be
Anish Rai <way2anishrai@gmail.com>
2026-07-17 20:17:12 +0530

    feat(stepper): add reusable analysis stepper

---
+++ frontend/app/new-analysis/page.tsx
@@ -0,0 +1,11 @@
+import Stepper from "@/components/stepper/Stepper";
+
+export default function NewAnalysisPage() {
+  return (
+    <div className="space-y-8">
+
+      <Stepper currentStep={1} />
+
+    </div>
+  );
+}
